98e55b12fa1096e69711e1d1185b3646f8324953
kabil <kabiljbeli011@gmail.com>
2022-01-17 23:21:32 +0100

    #comment initial commit

---
+++ projectMonitoringBE/src/main/java/com/pfe/projectMonitoringBE/ProjectMonitoringBeApplication.java
@@ -0,0 +1,13 @@
+package com.pfe.projectMonitoringBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMonitoringBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMonitoringBeApplication.class, args);
+	}
+
+}
---
+++ projectMonitoringBE/src/test/java/com/pfe/projectMonitoringBE/ProjectMonitoringBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfe.projectMonitoringBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMonitoringBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
